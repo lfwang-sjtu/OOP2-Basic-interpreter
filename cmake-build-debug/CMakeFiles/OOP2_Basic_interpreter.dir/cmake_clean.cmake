9bdce8fd4de5d1cfb14506917267ec9eed0ac981
lfwang-sjtu <wanglefeng@sjtu.edu.cn>
2020-12-18 22:44:39 +0800

    This is the first submit for this homework. Although I haven't write much yet, I will finish it very soon !

---
+++ cmake-build-debug/CMakeFiles/OOP2_Basic_interpreter.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OOP2_Basic_interpreter.dir/main.cpp.obj"
+  "OOP2_Basic_interpreter.exe"
+  "OOP2_Basic_interpreter.exe.manifest"
+  "OOP2_Basic_interpreter.pdb"
+  "libOOP2_Basic_interpreter.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OOP2_Basic_interpreter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
